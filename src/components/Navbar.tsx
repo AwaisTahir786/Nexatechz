00af93ceb919d8cb9432844638cb269bcf400579
AwaisTahir786 <tahirawais111@gmail.com>
2024-10-02 02:03:49 +0500

    push

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,11 +39,11 @@ function Navbar({ScrollGraphicsDesign,ScrollToBrands,ScrollToShopifySTore,Scroll
         {/* navbar content */}
         <div className='cursor-pointer hidden md:flex space-x-10 md:mr-10 items-center
          text-white bg-clip-text '>
-  <div onClick={ScrollWebsiteDesign} className='hover:text-[#39FF14] '>Website Design</div>
-  <div onClick={ScrollGraphicsDesign} className='hover:text-[#39FF14]'>Graphics Design</div>
-  <div onClick={ScrollToShopifySTore} className='hover:text-[#39FF14]'>Pricing</div>
-  <div onClick={ScrollToBrands} className='hover:text-[#39FF14]'>Process</div>
-  <Link href={"/pricing"} className='hover:text-[#39FF14]'>Pricing</Link>
+  <div onClick={ScrollWebsiteDesign} className='hover:text-[#39FF14] text-lg '>Website Design</div>
+  <div onClick={ScrollGraphicsDesign} className='hover:text-[#39FF14] text-lg'>Graphics Design</div>
+  <div onClick={ScrollToShopifySTore} className='hover:text-[#39FF14] text-lg'>Pricing</div>
+  <div onClick={ScrollToBrands} className='hover:text-[#39FF14] text-lg'>Process</div>
+  <Link href={"/pricing"} className='hover:text-[#39FF14] text-lg'>Pricing</Link>
 </div>
 
 
