ff5323986e512a439b1954c6224b81585564d516
AwaisTahir786 <tahirawais111@gmail.com>
2024-10-03 01:15:23 +0500

    push

--- src/components/Faq_update.tsx
+++ src/components/Faq_update.tsx
@@ -1,52 +1,50 @@
 import React from "react";
 import Image from "next/image";
-// import female from "@/";
-// import bolt from "/bolt.svg";
 
 const FAQ = () => {
   return (
     <div>
-      <h2 className="text-3xl md:text-4xl font-bold text-center mt-20 mb-20 text-[#39FF14] ">
+      <h2 className="text-3xl md:text-4xl font-bold text-center mt-10 md:mt-16 mb-12 md:mb-20 text-[#39FF14] ">
         Questions? Answers.
       </h2>
 
       <div
         id="faq"
-        className="lg:mt-[0rem] space-y-6 p-10 md:px-20 lg:px-32  pt-10 pb-20 md:mx-28 my-20 bg-gradient-to-t bg-[#131313] text-white rounded-2xl flex flex-col justify-start"
+        className="lg:mt-0 space-y-6 p-5 md:px-12 lg:px-32 pt-10 pb-20 md:mx-10 lg:mx-28 my-10 bg-gradient-to-t bg-[#131313] text-white rounded-2xl flex flex-col justify-start"
       >
         <div className="flex flex-col lg:flex-row items-start lg:space-x-20 lg:pt-10 space-y-10 lg:space-y-0">
-          <div className="grid md:w-2/3 divide-y divide-white/10">
+          <div className="grid md:w-full lg:w-2/3 divide-y divide-white/10">
             {[
               {
                 question: "Who's behind Supafast?",
                 answer:
-                  "Surprising fact: Supafast started as a one-person agency and has grown into a remote, global team. Collaborate directly with Namya, the founder and design engineer, and our talented professionals worldwide. We’ve worked with clients from 15+ countries, including Y Combinator startups, early-stage ventures, angel investors, and creators.",
+                  "Supafast started as a one-person agency and has grown into a remote, global team. Collaborate directly with Namya, the founder, and our talented professionals worldwide.",
               },
               {
                 question: "Why not hire a full-time designer and developer?",
                 answer:
-                  "Hiring full-time professionals can be a headache. It's costlier and more challenging to manage. A senior designer might set you back $100k+, while a developer could easily exceed $300k. That's a total of $500k+ when you factor in benefits, vacation days, and more.",
+                  "Hiring full-time professionals can be costly. A senior designer might cost $100k+, while a developer could exceed $300k, which adds up to over $500k annually.",
               },
               {
                 question: "What if I'm not happy with the results?",
                 answer:
-                  "This doesn't happen very often; if you're not happy, we offer unlimited revisions. We'll continue refining the project until you're 100% content.",
+                  "If you're not satisfied, we offer unlimited revisions until you're 100% happy with the outcome.",
               },
               {
                 question: "How quickly will I receive my project?",
                 answer:
-                  "True to our name, we're Supafast! Typically, most requests are fulfilled in just 48 hours or less. However, for more complex requests, the turnaround time may be a bit longer. Rest assured, we'll keep you informed every step of the way.",
+                  "Most requests are fulfilled within 48 hours, though complex projects may take a bit longer.",
               },
               {
                 question: "Can I get a refund?",
                 answer:
-                  "Although we are unable to provide refunds as we allocate dedicated time and resources to each client, we value flexibility. You have the option to pause your service at any time and resume it later, ensuring that you can fully benefit from our collaboration.",
+                  "We don't provide refunds, but you can pause and resume our services at any time to suit your needs.",
               },
             ].map(({ question, answer }, index) => (
               <div key={index} className="py-5">
                 <details className="group w-full">
                   <summary className="flex cursor-pointer list-none items-center justify-between space-x-5">
-                    <span className="lg:text-2xl text-white font-medium">
+                    <span className="lg:text-2xl md:text-xl text-white font-medium">
                       {question}
                     </span>
                     <span className="transition duration-300 group-open:rotate-45">
@@ -66,7 +64,7 @@ const FAQ = () => {
                     </span>
                   </summary>
                   <div className="pt-3">
-                    <span className="group-open:animate-fadeIn description text-start leading-7 w-1/2 text-sm font-light">
+                    <span className="group-open:animate-fadeIn description text-start leading-7 w-full md:w-4/5 text-sm font-light">
                       {answer}
                     </span>
                   </div>
@@ -77,45 +75,45 @@ const FAQ = () => {
 
           {/* Second Column */}
           <div className="relative lg:w-1/3">
-          <Image
-            alt="scribble"
-            loading="eager"
-            width="30"
-            height="50"
-            decoding="async"
-            data-nimg="1"
-            className="w-full"
-            src="/bolt.svg"
-          />
-          <div className="p-5 md:p-10 absolute top-0 left-0 flex flex-col items-center justify-center text-center w-full space-y-5">
-            <a href="https://twitter.com/namyakhann">
-              <Image
-                alt="scribble"
-                loading="eager"
-                width="30"
-                height="50"
-                decoding="async"
-                data-nimg="1"
-                className="w-[100px] xl:w-[150px] cursor-pointer"
-                src="/female.svg"
-              />
-            </a>
-            <div className="flex flex-col items-center justify-center">
-              <span className="lg:text-xl xl:text-xl text-white font-medium ">
-                Have more questions?
-              </span>
-              <span className="lg:text-xl xl:text-xl text-white font-medium ">
-                Book a FREE intro call.
-              </span>
+            <Image
+              alt="scribble"
+              loading="eager"
+              width="30"
+              height="50"
+              decoding="async"
+              data-nimg="1"
+              className="w-full"
+              src="/bolt.svg"
+            />
+            <div className="p-5 md:p-8 absolute top-0 left-0 flex flex-col items-center justify-center text-center w-full space-y-5">
+              <a href="https://twitter.com/namyakhann">
+                <Image
+                  alt="scribble"
+                  loading="eager"
+                  width="30"
+                  height="50"
+                  decoding="async"
+                  data-nimg="1"
+                  className="w-[80px] md:w-[100px] xl:w-[150px] cursor-pointer"
+                  src="/female.svg"
+                />
+              </a>
+              <div className="flex flex-col items-center justify-center">
+                <span className="lg:text-xl md:text-lg xl:text-xl text-white font-medium ">
+                  Have more questions?
+                </span>
+                <span className="lg:text-xl md:text-lg xl:text-xl text-white font-medium ">
+                  Book a FREE intro call.
+                </span>
+              </div>
+              <a
+                href="https://cal.com/namya/15min"
+                className="rounded-xl bg-[#39FF14] text-black p-3.5 md:p-4 px-10 text-sm md:px-14 text-md lg:text-lg font-bold text-primary tracking-tight cta-btn cursor-pointer hover:scale-105 transition duration-300 ease-out"
+              >
+                Book a Call
+              </a>
             </div>
-            <a
-              href="https://cal.com/namya/15min"
-              className="rounded-xl bg-[#39FF14] text-black p-3.5 lg:p-4 px-10 lg:px-16 text-md lg:text-lg font-bold text-primary tracking-tight cta-btn cursor-pointer hover:scale-105 transition duration-300 ease-out"
-            >
-              Book a Call
-            </a>
           </div>
-        </div>
         </div>
       </div>
     </div>
